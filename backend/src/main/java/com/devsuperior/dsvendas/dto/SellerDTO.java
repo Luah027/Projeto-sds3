33731cbaa7aa30598d6d42562d10ed069c944b71
Luana Carolina <luana.maximo027@gmail.com>
2021-05-08 23:13:39 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -36,7 +36,4 @@ public class SellerDTO implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	
 }
--- backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsvendas.service;
+package com.devsuperior.dsvendas.services;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +16,7 @@ public class SellerService {
 	@Autowired
 	private SellerRepository repository;
 	
-	public List<SellerDTO> findAll() {
+	public List<SellerDTO> findAll(){
 		List<Seller> result = repository.findAll();
 		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
 	}
